8c35a49ef9b567bdcaa2fd98ec54d1e3fda71211
Ramiro Gómez <code@ramiro.org>
2013-09-11 18:31:25 +0200

    first commit

---
+++ fabfile.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from fabric.api import local, lcd
+
+COUNTRY = 'DEU'
+DATA_DIR = 'data'
+SHP = 'ne_10m_admin_1_states_provinces_shp'
+SHP_SHP = '%s.shp' % SHP
+SHP_ZIP = '%s.zip' % SHP
+
+
+def dl():
+    local('curl -L -o %s/%s http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/%s' % (SHP_ZIP, DATA_DIR, SHP_ZIP))
+
+
+def extract():
+    with lcd(DATA_DIR):
+        local('unzip %s' % SHP_ZIP)
+
+def geojson():
+    with lcd(DATA_DIR):
+        local('ogr2ogr -f GeoJSON -where "sr_adm0_a3 IN (\'%s\')" subunits.json %s' % (COUNTRY, SHP_SHP))
+
+
+def topojson():
+    with lcd(DATA_DIR):
+        #FIXME use "code_hasc": "DE.NW"?
+        local('topojson --id-property su_a3 -p NAME=name -p name -o %s.topo.json subunits.json' % COUNTRY)
+        local('mv subunits.json %s.geo,json' % COUNTRY)
+
+
+def json():
+    geojson()
+    topojson()
+
+
+def deploy():
+    local('logya gen')
+    local('rsync -aruvz deploy/ hm:~/www/sandbox/')
---
+++ static/js/maps/germany.js
@@ -0,0 +1,61 @@
+// based on https://milkator.wordpress.com/2013/02/25/making-a-map-of-germany-with-topojson/
+
+var width = 675,
+    height = 900;
+
+var path = d3.geo.path();
+
+var svg = d3.select('#main').append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+d3.json('/data/DEU.topo.json', showData);
+
+function showData(error, de) {
+    var subunits = topojson.object(de, de.objects.subunits);
+
+    var projection = d3.geo.mercator()
+        .center([10.5, 51.35])
+        .scale(3800)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection)
+        .pointRadius(4);
+
+    svg.append('path')
+        .datum(subunits)
+        .attr('d', path)
+
+    svg.selectAll('.subunit')
+        .data(topojson.object(de, de.objects.subunits).geometries)
+      .enter().append('path')
+        .attr('class', function(d) { return 'subunit ' + d.properties.name; })
+        .attr('d', path)
+        .on('click', click);
+
+    function click(a){
+        console.log(a.properties.name);}
+
+    svg.append('path')
+        .datum(topojson.mesh(de, de.objects.subunits, function(a,b) { if (a!==b || a.properties.name === 'Berlin'|| a.properties.name === 'Bremen'){var ret = a;}return ret;}))
+        .attr('d', path)
+        .attr('class', 'subunit-boundary');
+
+    svg.selectAll('.subunit-label')
+        .data(topojson.object(de, de.objects.subunits).geometries)
+      .enter().append('text')
+        .attr('class', function(d) { return 'subunit-label ' + d.properties.name; })
+        .attr('transform', function(d) { return 'translate(' + path.centroid(d) + ')'; })
+        .attr('dy', function(d){
+//console.log(d.properties)
+            if(d.properties.name==='Brandenburg') {
+                return '2em'
+            }
+            if(d.properties.name==='Sachsen-Anhalt' || d.properties.name==='Rheinland-Pfalz' || d.properties.name==='Bremen') {
+                return '1em'
+            }
+            return '.35em';
+        })
+        .text(function(d) { return d.properties.name; });
+}
